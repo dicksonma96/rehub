376da73f54b81396f053091083257106974c1a15
Dickson <dickson.ma@claritas.asia>
2026-01-09 13:20:47 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    // This ensures that generated assets use relative paths
+    // instead of absolute paths where possible
+    assets: "_astro",
+  },
+});
