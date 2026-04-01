b361a2ced4ad78307939a32e5ed5a622d107193e
Dickson <dicksonma96@gmail.com>
2026-04-01 22:55:23 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-// @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://re-hub.com.my/",
+  integrations: [sitemap()],
   build: {
     // This ensures that generated assets use relative paths
     // instead of absolute paths where possible
